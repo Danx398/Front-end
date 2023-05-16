e619385c1de0dfa3719f68af46285e90ef0e04c9
Danx398 <danidannniel398@gmail.com>
2023-05-16 00:38:27 -0600

    practicaVideos

---
+++ Angular/proyectoPractica/src/app/dbz/pages/main-page/main-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../../interfaces/persona.interface'
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+  public personajes: Personaje[] = [
+    { nombre: 'Goku', poder: 20000 }
+  ];
+  public obtenNuevoPersonaje(personaje: Personaje) {
+   console.log(personaje);
+  }
+}
